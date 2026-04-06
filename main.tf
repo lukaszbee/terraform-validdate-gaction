aee143fad80462c1a67d7078c781a353e977e4ed
lukaszbee <deuxba@gmail.com>
2026-04-06 19:02:02 +0200

    dodanie bloku data

--- main.tf
+++ main.tf
@@ -12,3 +12,10 @@ resource "null_resource" "example" {
     always_run = "${timestamp()}"
   }
 }
+
+data "local_file" "dane_tymczasowe" {
+  for_each = var.pliki_tekstowe
+
+  depends_on = [terraform_data.echo_tresc]
+  filename   = each.value.sciezka_tymczasowa
+}
